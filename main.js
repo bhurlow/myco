9913700418436f5f8ece3154098b6f04dd6d0445
Brian Hurlow <brian@brianhurlow.com>
2023-01-14 18:07:44 -0500

    try pg connection

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("test result"));
+const { Client } = require("pg");
+const client = new Client();
+
+client.connect(process.env.PG_URL);
+
+app.get("/", async (req, res) => {
+  try {
+    console.log("ENV", process.env);
+
+    const res = await client.query("SELECT $1::text as message", [
+      "Hello world!",
+    ]);
+
+    res.end(JSON.stringify(res.rows));
+  } catch (e) {
+    res.send(e.toString());
+  }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
